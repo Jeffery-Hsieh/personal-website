b562754a0ad5fbe83321846826177fabf10d3bc8
Jeffery Hsieh <jefferyhsieh1996@gmail.com>
2022-10-30 13:39:48 +0800

    feat: add home page

---
+++ components/Thumbnail.tsx
@@ -0,0 +1,44 @@
+import {
+  Box,
+  Flex,
+  LinkBox,
+  AspectRatio,
+  Image,
+  Text,
+  LinkOverlay,
+} from "@chakra-ui/react";
+
+type ThumbnailProps = {
+  url: string;
+  image: string;
+  description: string;
+};
+
+const Thumbnail = ({ url, image, description }: ThumbnailProps) => {
+  return (
+    <LinkBox as="article">
+      <AspectRatio
+        ratio={2 / 1}
+        borderWidth="1px"
+        borderStyle="solid"
+        borderColor="gray.300"
+        borderRadius="xl"
+        overflow="hidden"
+      >
+        <Image src={image} />
+      </AspectRatio>
+      <LinkOverlay href={url}>
+        <Text
+          display={{ base: "none", md: "block" }}
+          px="8%"
+          textAlign="center"
+          mt="4"
+        >
+          {description}
+        </Text>
+      </LinkOverlay>
+    </LinkBox>
+  );
+};
+
+export default Thumbnail;
