099e2c500a725f104082e21257de8362f9a3f16d
Jeffery Hsieh <jefferyhsieh1996@gmail.com>
2023-01-27 15:34:45 +0800

    feat: add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,32 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+    return (
+        <>
+            <ChakraProvider>
+                <Component {...pageProps} />
+            </ChakraProvider>
+            <Script
+                strategy="lazyOnload"
+                src="https://www.googletagmanager.com/gtag/js?id=G-G68ZSSELWT"
+            ></Script>
+            <Script
+                id="google-analytics"
+                strategy="lazyOnload"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', 'G-G68ZSSELWT');
+                `,
+                }}
+            />
+        </>
+    );
 }
 
 export default MyApp;
